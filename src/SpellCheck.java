635f4bf3f5cf0bbd0f5aa55a1678ddc321bd6fe4
zoe-704 <sunzoey@gmail.com>
2025-10-04 20:21:36 -0700

    Trie implementation

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -5,37 +5,51 @@ import java.util.*;
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
- *
  * Completed by: Zoe Sun
  * */
 
 // trie and node implementation
-public class Node {
+class Node {
     boolean isWord;
     Node[] next;
     public Node() {
         isWord = false;
-    }
-    boolean isWord() {
-        return isWord;
-    }
-    Node[] getNext() {
-        return next;
+        next = new Node[27]; // a-z + '
     }
     void setWord() {
-        // todo
+        isWord = true;
     }
+
 }
-public class Trie {
+class Trie {
     Node root;
     public Trie () {
-        root =
+        root = new Node();
     }
+
+    private int letterToIndex(char letter) {
+        if (letter == '\'') return 26;
+        if (letter >= 'a' && letter <= 'z') return letter - 'a';
+        return -1; // not valid
+    }
+
     void insert(String s) {
-
+        Node cur = root;
+        for (char letter : s.toCharArray()) {
+            int index = letterToIndex(letter);
+            if (cur.next[index] == null) cur.next[index] = new Node();
+            cur = cur.next[index];
+        }
+        cur.setWord();
     }
-    Node lookup() {
-
+    boolean lookup(String s) {
+        Node cur = root;
+        for (char letter : s.toCharArray()) {
+            int index = letterToIndex(letter);
+            if (cur.next[index] == null) return false;
+            cur = cur.next[index];
+        }
+        return cur.isWord;
     }
 }
 
@@ -43,18 +57,31 @@ public class SpellCheck {
     /**
      * checkWords finds all words in text that are not present in dictionary
      *
-     * @param text The list of all words in the text.
+     * @param text       The list of all words in the text.
      * @param dictionary The list of all accepted words.
      * @return String[] of all misspelled words in the order they appear in text. No duplicates.
      */
-
+    // 560 ms, 564 ms, 523 ms with trie
     public String[] checkWords(String[] text, String[] dictionary) {
-        // make a trie;
-
-        return ;
+        // Initialize new trie and insert words into it
+        Trie trie = new Trie();
+        for (String word : dictionary) {
+            trie.insert(word);
+        }
+        // Initialize an ordered misspelled set and add words to it that are not in dictionary
+        Set<String> misspelled = new LinkedHashSet<>();
+        for (String word : text) {
+            if (!trie.lookup(word)) {
+                misspelled.add(word);
+            }
+        }
+        // Return misspelled set as an array
+        return misspelled.toArray(new String[0]);
     }
+}
+/*//approach using a linked hashet and hashset 483, 447, 436 ms
+public class SpellCheck {
 
-    /* approach using a linked hashet and hashset
     public String[] checkWords(String[] text, String[] dictionary) {
         Set<String> dict = new HashSet<>(Arrays.asList(dictionary)); // 992 ms, 957 ms, 1 s 23 ms
         Set<String> misspelled = new LinkedHashSet<>(); // ordered
@@ -69,7 +96,7 @@ public class SpellCheck {
         }
         return misspelled.toArray(new String[0]);
     }
-     */
+
 
     /* binary search approach
     // takes 3s 49 ms, 117ms, 160 ms
@@ -88,5 +115,4 @@ public class SpellCheck {
         }
         return -1;
     }
-    */
-}
+} */
